5573f4868be04480964c215b1ae4ae01eeb50860
jspr-rnty-jr <jasperjr@rantay@gmail.com>
2024-08-06 01:35:53 +0800

    refactor the entire portfolio page

--- js/app.js
+++ js/app.js
@@ -2,3 +2,118 @@
 particlesJS.load('particles-js', 'assets/particles.json', function() {
     console.log('callback - particles.js config loaded');
 });
+
+// Scroll reveal
+const defaultProps = {
+    easing: "cubic-bezier(0.5, 0, 0, 1)",
+    distance: "30px",
+    duration: 1000,
+    desktop: true,
+    mobile: true,
+  };
+
+  const targetElements = [
+    {
+        element: ".logo-img",
+        animation: {
+            delay: 200,
+            distance: "0px"
+        }
+    },
+    {
+      element: ".section-title",
+      animation: {
+        delay: 300,
+        distance: "0px",
+        origin: "bottom",
+      },
+    },
+    {
+      element: ".hero-title",
+      animation: {
+        delay: 500,
+        origin: window.innerWidth > 768 ? "left" : "bottom",
+      },
+    },
+    {
+        element: ".hero-tagline",
+        animation: {
+          delay: 750,
+          origin: window.innerWidth > 768 ? "left" : "bottom",
+        },
+    },
+    {
+      element: ".hero-cta",
+      animation: {
+        delay: 1000,
+        origin: window.innerWidth > 768 ? "left" : "bottom",
+      },
+    },
+    {
+      element: ".about-wrapper__image",
+      animation: {
+        delay: 600,
+        origin: "bottom",
+      },
+    },
+    {
+      element: ".about-wrapper__info",
+      animation: {
+        delay: 1000,
+        origin: window.innerWidth > 768 ? "left" : "bottom",
+      },
+    },
+    {
+      element: ".project-wrapper__text",
+      animation: {
+        delay: 500,
+        origin: window.innerWidth > 768 ? "left" : "bottom",
+      },
+    },
+    {
+      element: ".project-wrapper__image",
+      animation: {
+        delay: 1000,
+        origin: window.innerWidth > 768 ? "right" : "bottom",
+      },
+    },
+    {
+      element: ".contact-wrapper",
+      animation: {
+        delay: 800,
+        origin: "bottom",
+      },
+    },
+  ];
+
+const initScrollReveal = (targetElements, defaultProps) => {
+    if (!targetElements.length) return;
+
+    ScrollReveal({ reset: false });
+
+    targetElements.forEach(({ element, animation }) => {
+        ScrollReveal().reveal(element, Object.assign({}, defaultProps, animation));
+    });
+}
+
+initScrollReveal(targetElements, defaultProps);
+
+// ScrollReveal({ reset: true });
+// ScrollReveal().reveal('.about-wrapper__image, .section-title, .hero-content', { duration: 2000 });
+
+
+const navbarToggle = navbar.querySelector('#navbar-toggle');
+let isNavbarExpanded = navbarToggle.getAttribute('aria-expanded') === 'true';
+
+const toggleNavbarVisibility = () => {
+  isNavbarExpanded = !isNavbarExpanded;
+  navbarToggle.setAttribute('aria-expanded', isNavbarExpanded);
+};
+
+navbarToggle.addEventListener('click', toggleNavbarVisibility);
+
+const navbarMenu = document.querySelector('#navbar-menu');
+const navbarLinksContainer = navbarMenu.querySelector('.navbar-links');
+
+navbarLinksContainer.addEventListener('click', (e) => e.stopPropagation());
+navbarMenu.addEventListener('click', toggleNavbarVisibility);
